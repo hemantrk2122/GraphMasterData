2ba4ecfbb4fbbe8c538f33329b7df9262c04f5e6
hemantrk2122 <hemantrk2122@gmail.com>
2023-01-06 01:55:54 +0530

    vs code setup

--- src/com/graph/Graph.java
+++ src/com/graph/Graph.java
@@ -5,30 +5,33 @@ import java.util.*;
 public class Graph {
 	public static void addEdge(int u,int v,int w, ArrayList<Edge>[] graph) {
 		graph[u].add(new Edge(u,v,w));
-//		graph[v].add(new Edge(v,u,w));
+		graph[v].add(new Edge(v,u,w));
 	}
 	public static void display(ArrayList<Edge>[] graph) {
 		for(int i = 0;i<graph.length;i++) {
 			for(Edge e:graph[i]){
-				System.out.println(e.u+ +e.v+ +e.w);
+				System.out.println(e.u+" "+e.v+" "+e.w);
 			}
 		}
 	}
 	public static void main(String[] args) {
-//		Scanner scn = new Scanner(System.in);
-//		int vtces = scn.nextInt();
-//		ArrayList<Edge>[] graph = new ArrayList[vtces];
-//		for(int i = 0;i<vtces;i++) {
-//			graph[i] = new ArrayList<>();
-//		}
-//		int edge_count = scn.nextInt();
-//		for(int i = 0;i<edge_count;i++) {
-//			int u = scn.nextInt();
-//			int v = scn.nextInt();
-////			int w = scn.nextInt();
-//			addEdge(u,v,0,graph);
-//		}
-		ThreeSum instance = new ThreeSum();
-		instance.solve();
+		Scanner scn = new Scanner(System.in);
+		int vtces = scn.nextInt();
+		ArrayList<Edge>[] graph = new ArrayList[vtces];
+		for(int i = 0;i<vtces;i++) {
+			graph[i] = new ArrayList<>();
+		}
+		int edge_count = scn.nextInt();
+		for(int i = 0;i<edge_count;i++) {
+			int u = scn.nextInt();
+			int v = scn.nextInt();
+			int w = scn.nextInt();
+			addEdge(u,v,w,graph);
+		}
+		int src = scn.nextInt();
+		int dest = scn.nextInt();
+		display(graph);
+		isPath instance = new isPath();
+		System.out.println(instance.ispath(graph,src,dest,new boolean[vtces]));
 	}
 }
